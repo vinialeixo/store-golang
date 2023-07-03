e2347bcb65a145747323b49c5e20a34b7134f655
Vinicius Dias <vi_aleixodias@hotmail.com>
2023-07-03 14:44:28 -0300

    feat: add struct in the code and make dynamic

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+type Products struct {
+	Nome        string
+	Description string
+	Price       float64
+	Qnt         int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -14,5 +21,32 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	products := []Products{
+		{
+			Nome:        "Camiseta",
+			Description: "Descrição do Produto 1",
+			Price:       9.99,
+			Qnt:         5,
+		},
+		{
+			Nome:        "Shorts",
+			Description: "Descrição do Produto 2",
+			Price:       19.99,
+			Qnt:         10,
+		},
+		{
+			Nome:        "Meia",
+			Description: "Descrição do Produto 3",
+			Price:       14.99,
+			Qnt:         3,
+		},
+		{
+			Nome:        "Chuteira",
+			Description: "Descrição do Produto 4",
+			Price:       24.99,
+			Qnt:         8,
+		},
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
 }
