b2bf7ead0045700d7287d7bcc7fee62d35ce2533
Vinicius Dias <vi_aleixodias@hotmail.com>
2023-07-03 14:49:36 -0300

    feat: add descriptions

--- main.go
+++ main.go
@@ -24,25 +24,25 @@ func index(w http.ResponseWriter, r *http.Request) {
 	products := []Products{
 		{
 			Nome:        "Camiseta",
-			Description: "Descrição do Produto 1",
+			Description: "Descrição da Camiseta: Esta camiseta de algodão de alta qualidade é perfeita para uso diário. Disponível em diferentes tamanhos e cores.",
 			Price:       9.99,
 			Qnt:         5,
 		},
 		{
 			Nome:        "Shorts",
-			Description: "Descrição do Produto 2",
+			Description: "Descrição dos Shorts: Estes shorts leves são ideais para atividades esportivas. Feitos de um material resistente e confortável.",
 			Price:       19.99,
 			Qnt:         10,
 		},
 		{
 			Nome:        "Meia",
-			Description: "Descrição do Produto 3",
+			Description: "Descrição da Meia: Esta meia macia e respirável mantém os pés confortáveis durante o dia todo. Disponível em diferentes cores e tamanhos.",
 			Price:       14.99,
 			Qnt:         3,
 		},
 		{
 			Nome:        "Chuteira",
-			Description: "Descrição do Produto 4",
+			Description: "Descrição da Chuteira: Esta chuteira de alta performance oferece excelente aderência e estabilidade no campo. Ideal para jogadores profissionais.",
 			Price:       24.99,
 			Qnt:         8,
 		},
